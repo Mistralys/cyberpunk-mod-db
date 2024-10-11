ea92af048cede8869b99d573243c35356349385a
Mistralys <s.mordziol@gmail.com>
2024-10-11 08:34:45 +0200

    Script to generate the mods list.

---
+++ bin/generate-mods-list.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Script used to generate the `modslist.md` file from the JSON files.
+ *
+ * @package CPMDB
+ */
+
+use AppUtils\ConvertHelper;
+
+require_once __DIR__ . '/prepend.php';
+
+$list = array();
+foreach(getFiles() as $file) {
+    $data = $file->parse();
+    $list[] = array(
+        'label' => $data['mod'],
+        'url' => $data['url'],
+        'authors' => $data['authors']
+    );
+}
+
+usort($list, function(array $a, array $b) : int {
+    return strnatcasecmp($a['label'], $b['label']);
+});
+
+$lines = array();
+
+foreach($list as $entry) {
+    $lines[] = sprintf(
+        '* [%s](%s) by %s',
+        $entry['label'],
+        $entry['url'],
+        ConvertHelper::implodeWithAnd($entry['authors'], ', ', ' and ')
+    );
+}
+
+file_put_contents(
+    __DIR__.'/../modslist.md',
+    '## Mods List'.PHP_EOL.PHP_EOL.
+    implode(PHP_EOL, $lines).PHP_EOL
+);
