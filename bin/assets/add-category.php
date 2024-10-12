96f8027b9d1efbec84e6221416f83f18184e0809
Mistralys <s.mordziol@gmail.com>
2024-10-12 19:09:49 +0200

    Create add-category.php

---
+++ bin/assets/add-category.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use AppUtils\FileHelper_Exception;
+
+require_once __DIR__.'/prepend.php';
+
+/**
+ * @param string $modID
+ * @param array<string,string> $commands
+ * @return void
+ * @throws FileHelper_Exception
+ */
+function addCategory(string $modID, array $commands) : void
+{
+    $label = $commands['label'] ?? null;
+
+    $file = getModFile($modID);
+    $data = $file->parse();
+
+    $data['itemCategories'][] = getCategorySkeleton($label);
+
+    $file->putData($data, true);
+}
