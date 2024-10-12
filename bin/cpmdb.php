79e02cf7e81ca05a3d4a82a8297d19e6d66416c5
Mistralys <s.mordziol@gmail.com>
2024-10-12 19:10:03 +0200

    Added `add-category` command.

--- bin/cpmdb.php
+++ bin/cpmdb.php
@@ -9,6 +9,9 @@ function showUsage() : void
     logInfo('# Create a new mod file skeleton');
     logInfo('php cpmdb.php create=mod_id name="Mod name"');
     logInfo('');
+    logInfo('# Add a new category to a mod file');
+    logInfo('php cpmdb.php add-category=mod_id label="Category label"');
+    logInfo('');
     logInfo('# Normalize all mod files');
     logInfo('php cpmdb.php normalize');
     logInfo('');
@@ -37,8 +40,19 @@ if(isset($commands['modslist']) || isset($commands['modlist'])) {
     exit;
 }
 
-if(isset($commands['create'])) {
+$createID = $commands['create'] ?? null;
+if(!empty($createID)) {
     require_once __DIR__ . '/assets/create-new.php';
-    createNew($commands);
+    createNew($createID, $commands);
     exit;
 }
+
+$addCategoryID = $commands['add-category'] ?? $commands['addc'] ?? null;
+if(!empty($addCategoryID)) {
+    require_once __DIR__ . '/assets/add-category.php';
+    addCategory($addCategoryID, $commands);
+    exit;
+}
+
+showUsage();
+exit;
