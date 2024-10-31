4410f615851e426f34baa0e0597b9dead5006ea5
Mistralys <s.mordziol@gmail.com>
2024-10-31 18:40:42 +0100

    Added a build script.

---
+++ bin/assets/build-release.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CPMB\Assets;
+
+use function CPMDB\Assets\checkScreenshots;
+use function CPMDB\Assets\generateModsList;
+use function CPMDB\Assets\generateTagsReference;
+use function CPMDB\Assets\getCLICommands;
+
+function getBuildReleaseArg() : ?string
+{
+    $commands = getCLICommands();
+
+    return
+        $commands['build-release'] ??
+        $commands['build'] ??
+        $commands['buildrelease'] ??
+        $commands['br'] ??
+        null;
+}
+
+function buildRelease() : void
+{
+    generateModsList();
+    generateTagsReference();
+    checkScreenshots();
+}
