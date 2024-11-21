4f5b08e30e1ea45d4e0262c9d544525c0e14ea40
Mistralys <s.mordziol@gmail.com>
2024-11-21 18:19:02 +0100

    Added known search terms list.

---
+++ bin/assets/search-terms.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CPMDB\Assets;
+
+const SEARCH_TERMS = array(
+    'Adshield' => 'shield',
+    'CaffeinatedRogue' => 'rogue',
+    'CaffeineRogue' => 'rogue',
+    'Cyb3rbling' => 'cyber bling',
+    'EzioMaverick' => 'maverick',
+    'MONSTERaider' => 'raider',
+    'NolaDreamer' => 'dreamer',
+    'PeachuHime' => 'hime',
+    'PinkyDude' => 'dude',
+    'Rvcoon' => 'raccoon',
+    'ScorpionTank' => 'tank',
+    'Se7en' => 'seven',
+    'TheLoneStrider' => 'lone strider',
+    'VV0LFDEN' => 'wolf den',
+    'VeegeeAlvarez' => 'alvarez',
+    'beaniebby' => 'baby',
+    'cyb3rsn4k3' => 'cyber snake',
+    'cyb3rsn4k34li3n' => 'cyber snake alien',
+    'deadengine959' => 'engine 959',
+    'mistshield' => 'shield',
+    'yellingintothevoid' => 'into void',
+);
