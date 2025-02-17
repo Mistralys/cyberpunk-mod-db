c60d1928063dbb57f7d89c31aa0805e1d6568dc7
Mistralys <s.mordziol@gmail.com>
2025-02-17 17:02:06 +0100

    Improved screenshot checking.

--- bin/assets/check-screenshots.php
+++ bin/assets/check-screenshots.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace CPMDB\Assets;
 
+use AppUtils\FileHelper;
+use AppUtils\FileHelper\JSONFile;
+
+const GLOBAL_SCREEN_SIDECAR_FILES = '__cpmdb_screen_sidecar_files';
+
+/**
+ * Mods where not having a screenshot for the
+ * MaleV version can be ignored.
+ */
+const IGNORE_MOD_MALE_V_SCREENSHOTS = array(
+    'xtx-sneakers',
+    'biker-boots',
+    'leather-boots',
+    'military-boots'
+);
+
 function getCheckScreenshotsArg() : ?string
 {
     $commands = getCLICommands();
@@ -21,22 +37,96 @@ function checkScreenshots() : void
 {
     $screenshotPath = __DIR__ . '/../../data/clothing/screens';
 
-    foreach(getFiles() as $file) {
-        $path = sprintf(
-            '%s/%s.jpg',
-            $screenshotPath,
-            $file->getBaseName()
-        );
+    foreach(getModFiles() as $file) {
+        checkModScreenshots($file->getBaseName(), $screenshotPath);
+    }
+}
 
-        if(!file_exists($path)) {
+function checkModScreenshots(string $modID, string $screenshotPath) : void
+{
+    $path = sprintf(
+        '%s/%s.jpg',
+        $screenshotPath,
+        $modID
+    );
+
+    if(!file_exists($path)) {
+        addMessage(
+            'Missing screenshot',
+            'mod ['.$modID.']'
+        );
+        logError(
+            'Missing screenshot for [%s]',
+            $modID
+        );
+    }
+
+    $sidecarFiles = getModScreenshotSidecarFiles($modID);
+    $tags = getModTags($modID);
+
+    // Check if mods that support MaleV have a screenshot for that version
+    if(in_array('MaleV', $tags) && in_array('FemV', $tags) && !in_array($modID, IGNORE_MOD_MALE_V_SCREENSHOTS)) {
+        $found = false;
+        foreach($sidecarFiles as $sidecarFile) {
+            $data = $sidecarFile->getData();
+            if(in_array('MaleV', $data[KEY_SCREENSHOT_TAGS] ?? array())) {
+                $found = true;
+                break;
+            }
+        }
+
+        if(!$found) {
             addMessage(
-                'Missing screenshot',
-                'mod ['.$file->getBaseName().']'
+                'No screenshot detected for the MaleV version for mod [%s].',
+                $modID
             );
-            logError(
-                'Missing screenshot for [%s]',
-                $file->getBaseName()
+
+            logInfo(
+                'No screenshot detected for the MaleV version for mod [%s].',
+                $modID
             );
         }
     }
+}
+
+/**
+ * @param string $modID
+ * @return JSONFile[]
+ */
+function getModScreenshotSidecarFiles(string $modID) : array
+{
+    $files = getScreenshotSidecarFiles();
+    $result = array();
+
+    foreach($files as $baseName => $file) {
+        if(str_starts_with($baseName, $modID)) {
+            $result[] = $file;
+        }
+    }
+
+    return $result;
+}
+
+/**
+ * @return array<string, JSONFile>
+ */
+function getScreenshotSidecarFiles() : array
+{
+    if(isset($GLOBALS[GLOBAL_SCREEN_SIDECAR_FILES])) {
+        return $GLOBALS[GLOBAL_SCREEN_SIDECAR_FILES];
+    }
+
+    $files = FileHelper::createFileFinder(__DIR__.'/../../data/clothing/screens')
+        ->includeExtension('json')
+        ->getFiles()
+        ->typeJSON();
+
+    $result = array();
+    foreach($files as $file) {
+        $result[$file->getBaseName()] = $file;
+    }
+
+    $GLOBALS[GLOBAL_SCREEN_SIDECAR_FILES] = $result;
+
+    return $result;
 }
