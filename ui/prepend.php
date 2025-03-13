51e58ea993d8664292d88e48ba7601563c8b2b6e
Mistralys <s.mordziol@gmail.com>
2025-03-13 22:11:56 +0100

    Using the bootstrapper.

--- ui/prepend.php
+++ ui/prepend.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace CPMDB\UI;
 
+use const CPMDB\Config\CPMDB_SOURCES_FOLDER;
+
 const UI_ROOT = __DIR__;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../boostrap.php';
+
+if(CPMDB_SOURCES_FOLDER === '') {
+    die(sprintf('To use the pose pack UI, the %s constant must be defined in the config.php file.', 'CPMDB\Config\CPMDB_SOURCES_FOLDER'));
+}
