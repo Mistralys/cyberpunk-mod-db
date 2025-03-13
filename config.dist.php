03bc53b78e043ff4c246593f7489f926f158169c
Mistralys <s.mordziol@gmail.com>
2025-03-13 22:10:26 +0100

    Added configuration file template.

---
+++ config.dist.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Configuration file template, needed when authoring
+ * the database. For accessing the source pose pack
+ * screenshots, for example.
+ *
+ * @package CPMDB
+ * @subpackage Config
+ */
+
+declare(strict_types=1);
+
+namespace CPMDB\Config;
+
+/**
+ * This is used only if you are authoring pose packs.
+ * Absolute path to the local clone of the repository
+ * containing the DB sources.
+ *
+ * @link https://github.com/Mistralys/cyberpunk-mod-db-sources
+ */
+const CPMDB_SOURCES_FOLDER = '';
